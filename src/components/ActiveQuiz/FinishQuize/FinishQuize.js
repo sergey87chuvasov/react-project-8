7da4de9c13eb828383487265e985f449fa994fbf
Sergey87 <serge.techno@gmail.com>
2022-07-09 00:47:03 +0300

    added button component

--- src/components/ActiveQuiz/FinishQuize/FinishQuize.js
+++ src/components/ActiveQuiz/FinishQuize/FinishQuize.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './FinishQuize.module.css';
+import Button from '../../UI/Button/Button';
 
 const FinishQuize = (props) => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -41,7 +42,10 @@ const FinishQuize = (props) => {
         Правильно {successCount} из {props.quiz.length}
       </p>
       <div>
-        <button onClick={props.onRetry}>Повторить</button>
+        <Button onClick={props.onRetry} type='primary'>
+          Повторить
+        </Button>
+        <Button type='success'>Перейти в список тестов</Button>
       </div>
     </div>
   );
